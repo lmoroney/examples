28d1375e3dad9debbbc41389edc67f8177996bf6
jamestamplin <jamestamplin@users.noreply.github.com>
2025-02-24 18:01:12 -0800

    update agent.py and notebook

--- agents/llm/agent.py
+++ agents/llm/agent.py
@@ -1,77 +1,81 @@
-# this agent uses an LLM to decide what to do in a session
-# it uses the openrouter API to get the LLM response
+# this agent uses an LLM to decide what to do.
+# it uses the OpenRouter API to select, prompt, and get the response from any number of LLMs
 
 from kradle import (
-   AgentManager,
-   MinecraftAgent,
+    AgentManager,
+    MinecraftAgent,
 )
 from kradle.models import MinecraftEvent
 from dotenv import load_dotenv
 import os
 import requests
 from prompts.config import (
-  conversing_prompt,
-  coding_prompt,
-  conversation_examples,
-  coding_examples,
-  creative_mode_prompt
+    conversing_prompt,
+    coding_prompt,
+    conversation_examples,
+    coding_examples,
+    creative_mode_prompt,
 )
 
 load_dotenv()
 
 
 # let's define a model and persona for our agent
-MODEL = "openai/gpt-4o" # refer to https://openrouter.ai/models for available models
-PERSONA = "you are a cool resourceful agent. you really want to achieve the task that has been given to you." # check out some other personas in prompts/config.py
+MODEL = "openai/gpt-4o"  # refer to https://openrouter.ai/models for all available models
+PERSONA = "you are a cool resourceful agent. you really want to achieve the task that has been given to you."  # check out some other personas in prompts/config.py
 
 # plus some additional settings:
-
-RESPOND_WITH_CODE = False # set this to true to have the llm generate runnable code instead of regular commands
-DELAY_AFTER_ACTION = 100 # minimal delay after an action is performed. increase this if the agent is too fast or if you want more time to see the agent's actions
+RESPOND_WITH_CODE = False  # set this to true to have the llm generate runnable code instead of regular commands
+DELAY_AFTER_ACTION = 100  # this adds a delay (in milliseconds) after an action is performed. increase this if the agent is too fast or if you want more time to see the agent's actions
 
 # your openrouter API key
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 
+# this is your agent class. It extends the MinecraftAgent class in the Kradle SDK
+# you pass this whole class into the AgentManager.serve() below
+# this lets Kradle manage the lifecycle of this agent.
+# Kradle can create multiple instances of your agent (eg. adding two of the same agent to a challenge)
+# each instance of this class is called a 'participant'
 class LLMBasedAgent(MinecraftAgent):
 
-    username = "llm-agent" # this is the username of the agent
-    display_name = "LLM Agent" # this is the display name of the agent
+    username = "llm-agent"  # this is the username of the agent (eg. kradle.ai/<my-username>/agents/<my-agent-username>)
+    display_name = "LLM Agent"  # this is the display name of the agent
     description = "This is an LLM-based agent that can be used to perform tasks in Minecraft."
 
-    # the is the first call that the agent gets when the session starts
-    # agent_config contains all the instructions for the agent, starting with the task
-    # the agent returns a list of events that it is interested in, which will later trigger the on_event function
+    # this method is called when the session starts
+    # challenge_info has variables, like challenge_info.task
+    # use the return statement of this method to send a list of in-game events you wish to listen to.
+    # these will trigger the on_event() function when they occur
     def init_participant(self, challenge_info):
 
-        # self.memory is a utility instantiated for you 
+        # self.memory is a utility instantiated for you
         # that can be used to store and retrieve information
-        # throughout the lifecycle of this participant. It is an instance of
-        # the StandardMemory class in the kradle SDK
+        # it persists across the lifecycle of this participant.
+        # It is an instance of the StandardMemory class in the Kradle SDK
 
-        #set task
+        # save the task to memory
         self.memory.task = challenge_info.task
 
-        #set message lists
+        # array to store LLM history
         self.memory.messages = []
 
-        #set chat history
+        # array to store in-game chat history
         self.memory.chat_history = []
 
-        #set agent modes (e.g. creative mode, self_preservation, etc)
+        # set Minecraft modes (e.g. creative mode, self_preservation, etc)
+        # TODO: Link to docs to explain more.
         self.memory.agent_modes = challenge_info.agent_modes
 
-        #set action dictionary
-        # here, we have commands and js_functions
+        # dictionaries to store all possible commands and javascript functions
         self.memory.commands = challenge_info.commands
         self.memory.js_functions = challenge_info.js_functions
 
-        print(f"Initializing agent for participant ID: {self.participant_id} with slug: {self.username}")
+        print(f"Initializing agent for participant ID: {self.participant_id} with username: {self.username}")
         print(f"Persona: {PERSONA}")
         print(f"Model: {MODEL}")
 
-        # let's log the init call to kradle using the
-        # built in self.log() method from the MinecraftAgent class
+        # self.log() lets us log information to the Kradle dashboard (left pane in the session viewer)
         # self.log(
         #     {
         #         "persona": PERSONA,
@@ -79,40 +83,49 @@ class LLMBasedAgent(MinecraftAgent):
         #         "respond_with_code": RESPOND_WITH_CODE
         #     }
         # )
-    
-        # Agent_config contains available_events
-        return {'listenTo': [MinecraftEvent.MESSAGE, MinecraftEvent.COMMAND_EXECUTED]}
 
-    # this function is called when an event occurs
-    # the agent returns an action to be performed
+        # tell Kradle what we want to listen to
+        return {"listenTo": [MinecraftEvent.MESSAGE, MinecraftEvent.COMMAND_EXECUTED]}
+
+    # this is called when an event happens
+    # we return our next action
     def on_event(self, observation):
+
+        # lets format to observation, so we can pass it to the LLM
         state_summary = self.format_state_summary(observation)
         print(f"\nState Summary:\n{state_summary}")
-        print ("Task: ", self.memory.task)
+        print("Task: ", self.memory.task)
 
+        # now we pass it to the LLM, get the next action, and send it to Kradle
         response = self.get_llm_response(state_summary, observation)
         print(f"Agent Response: {response}")
         print(f"Participant ID: {self.participant_id}")
-        
+
         return response
 
-
-    # this function converts an observation object into a string that can be used to build the prompt
+    # convert observation object => string, so we can build a LLM prompt
     def format_state_summary(self, observation):
+
+        # python array operator to extend/append the next message
         self.memory.chat_history.extend(observation.messages)
-        
+
         print(f"Messages: {self.memory.chat_history}")
 
-        chat_summary = '\n'.join(
-            f'{msg.sender}: {msg.message}' 
-            for msg in self.memory.chat_history[-10:]
-        ) if self.memory.chat_history else 'None'
+        # lets get the last 10 messages
+        chat_summary = (
+            "\n".join(f"{msg.sender}: {msg.message}" for msg in self.memory.chat_history[-10:])
+            if self.memory.chat_history
+            else "None"
+        )
 
-        inventory_summary = ', '.join([
-            f'{count} {name}' 
-            for name, count in observation.inventory.items()
-        ]) if observation.inventory else 'None'
+        # lets get everythign in our inventory
+        inventory_summary = (
+            ", ".join([f"{count} {name}" for name, count in observation.inventory.items()])
+            if observation.inventory
+            else "None"
+        )
 
+        # return a string with everything the LLM needs to know
         return (
             f"{observation.output if observation.output else 'Output: None'}\n\n"
             f"Chat: {chat_summary}\n\n"
@@ -129,42 +142,42 @@ class LLMBasedAgent(MinecraftAgent):
         else:
             prompt = conversing_prompt
 
-        #load task, persona, agent_modes, and commands from memory to build the prompt
+        # load task, persona, agent_modes, and commands from memory to build the prompt
         prompt = prompt.replace("$NAME", observation.name)
         prompt = prompt.replace("$TASK", self.memory.task)
         prompt = prompt.replace("$PERSONA", PERSONA)
         prompt = prompt.replace("$AGENT_MODE", str(self.memory.agent_modes))
 
-        if RESPOND_WITH_CODE:    
+        # we can respond with javascript or text
+        if RESPOND_WITH_CODE:
             prompt = prompt.replace("$CODE_DOCS", str(self.memory.js_functions))
             prompt = prompt.replace("$EXAMPLES", str(coding_examples))
         else:
-            prompt = prompt.replace("$COMMAND_DOCS", str(self.memory.commands)) 
+            prompt = prompt.replace("$COMMAND_DOCS", str(self.memory.commands))
             prompt = prompt.replace("$EXAMPLES", str(conversation_examples))
-            
-            if self.memory.agent_modes['mcmode']=='creative':
+
+            if self.memory.agent_modes["mcmode"] == "creative":
                 prompt = prompt.replace("$CREATIVE_MODE", creative_mode_prompt)
             else:
                 prompt = prompt.replace("$CREATIVE_MODE", "You are in survival mode.")
 
         return prompt
 
-    # this function send the prompt to the LLM and gets the response
+    # send the prompt to the LLM and get the response
     def get_llm_response(self, state_summary, observation):
+
+        # build the messages array with the last 5 messages
         messages = [
             {"role": "system", "content": self.format_system_prompt(observation)},
             *self.memory.messages[-5:],
-            {"role": "user", "content": state_summary}
+            {"role": "user", "content": state_summary},
         ]
 
         response = requests.post(
             "https://openrouter.ai/api/v1/chat/completions",
             headers={"Authorization": f"Bearer {OPENROUTER_API_KEY}"},
-            json={
-                "model": MODEL,
-                "messages": messages
-            },
-            timeout=30
+            json={"model": MODEL, "messages": messages},
+            timeout=30,
         ).json()
 
         print(f"Response: {response}")
@@ -174,8 +187,7 @@ class LLMBasedAgent(MinecraftAgent):
         else:
             content = "I'm sorry, I'm having trouble generating a response. Please try again later."
 
-
-        # logging the llm call using the built in self.log() method
+        # logging what we sent and recieved to the Kradle dashboard
         # self.log(
         #     {
         #         "prompt": messages,
@@ -183,19 +195,20 @@ class LLMBasedAgent(MinecraftAgent):
         #         "response": content
         #     }
         # )
-        
-        self.memory.messages.extend([
-            {"role": "user", "content": state_summary},
-            {"role": "assistant", "content": content}
-        ])
+
+        # append to the message history
+        self.memory.messages.extend(
+            [{"role": "user", "content": state_summary}, {"role": "assistant", "content": content}]
+        )
 
         if RESPOND_WITH_CODE:
             return {"code": content, "delay": DELAY_AFTER_ACTION}
         return {"command": content, "delay": DELAY_AFTER_ACTION}
 
-# This creates a web server and is available through a SSH tunnel
-# the agent will be served at "/llm-agent"
+
+# finally, lets serve our agent!
+# this creates a web server and an SSH tunnel (so our agent has a stable public URL)
 connection_info = AgentManager.serve(LLMBasedAgent, create_public_url=True)
 print(f"Started agent at URL: {connection_info}")
 
-# now go to app.kradle.ai and run this agent against a challenge
+# now go to app.kradle.ai and run this agent against a challenge!
